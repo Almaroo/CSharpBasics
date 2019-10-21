826a498308c7e7ef722dc6513994e5c97e72df45
Almaroo <mfxbarszcz@gmail.com>
2019-10-21 23:36:26 +0200

    Added Filtr

---
+++ Filtr.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Filtr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = $@"{Directory.GetCurrentDirectory()}\input.txt";
+            string outputPath = $@"{Directory.GetCurrentDirectory()}\output.txt";
+
+            string numberPattern = @"^[+-]?\d+(:?[.,]\d+)?$";
+            Regex numRegEx = new Regex(numberPattern);
+
+            if(File.Exists(inputPath))
+            {
+
+                if (File.Exists(outputPath))
+                {
+                    using (StreamWriter sw = new StreamWriter(outputPath))
+                    {
+                        using (StreamReader sr = new StreamReader(inputPath))
+                        {
+                            string row;
+                            do
+                            {
+                                row = sr.ReadLine();
+                                if (row != null && numRegEx.IsMatch(row))
+                                    sw.WriteLine(row);
+
+                            } while (row != null);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Plik {Path.GetFileName(outputPath)} nie istnieje\nAby wyjść naciśnij dowolny klawisz...");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Plik {Path.GetFileName(inputPath)} nie istnieje\nAby wyjść naciśnij dowolny klawisz...");
+                Console.ReadKey();
+                return;
+            }
+
+            
+
+            
+
+
+        }
+    }
+}
