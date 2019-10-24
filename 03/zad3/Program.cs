7ab53a8b5ae7c86f997ec35f4270df5a62ad35b4
Almaroo <mfxbarszcz@gmail.com>
2019-10-24 13:56:27 +0200

    Added PO_03 v3.

---
+++ 03/zad3/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_3
+{
+    public enum Plcie //enum przechowujący płcie
+    {
+        K=0,
+        M=1
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Osoba osoba = new Osoba() -> wyrzuci błąd ponieważ NIE MOŻNA TWORZYĆ INSTANCJI KLASY ABSTRAKCYJNEJ!
+           
+            KierownikZespolu adam = new KierownikZespolu("Adam", "Kowalski", "1990/07/01", "90070100211", Plcie.M, 5);
+            CzlonekZespolu beata = new CzlonekZespolu("Beata", "Nowak", "1992/10/22", "92102201347", Plcie.K, "projektant");
+            CzlonekZespolu jan = new CzlonekZespolu("Jan", "Janowski", "1992/03/15", "92031507772", Plcie.M, "programista");
+
+            Console.WriteLine(beata.ToString());
+            Console.WriteLine(jan.ToString());
+            Console.WriteLine(adam.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 03/zad3/WrongPESELException.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO_3
+{
+    //Tworzymy nową klasę naszego wyjątku dziedziczącą po klasie Exception
+    class WrongPESELException : Exception
+    {
+        //Przechwytujemy wiadomość podaną jako WrongPESELException(msg) i przekazujemy ją do klasy bazowej (Exception) za pomocą base(message)
+        public WrongPESELException(string message) : base(message)
+        {
+
+        }
+    }
+}
